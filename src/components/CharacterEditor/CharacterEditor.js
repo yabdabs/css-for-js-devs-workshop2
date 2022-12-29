639f475733ed6a070a2dab09cbc0830ee5bdd5ef
Abdul <abdul.k.khan75@gmail.com>
2022-12-29 13:58:25 -0500

    finish workshop exercises on my own

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,21 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+       <div
+          className={styles.background}
+        />
+
+<div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
+
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -78,16 +93,7 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
-      </div>
+  
     </main>
   );
 }
